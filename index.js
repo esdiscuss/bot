f031b38677b7d33db76750711634030e9b945afa
ForbesLindesay <forbes@lindesay.co.uk>
2013-04-07 15:53:41 +0100

    Refactor

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ var pipermail = require('pipermail');
 var filters = require('./lib/pipermail-filters');
 var output = require('./lib/pipermail-output');
 
-module.exports = execute;
-function execute(options) {
+exports = (module.exports = messages);
+exports.messages = messages;
+function messages(options) {
   var source = options.source || 'https://mail.mozilla.org/pipermail/es-discuss/';
   source = source.replace(/\/?$/g, '/');
   var age = options.age;
   var organisation = options.organisation;
   var dryRun = options['dry-run'] || options.dryRun;
 
-  var stream = pipermail(source, {progress: false, cache: true})
-    .pipe(filters.spam())
-    .pipe(filters.fixSubjects())
-    .pipe(filters.fixDates())
-    .pipe(filters.fixID());
+  var stream = pipermail(source, {progress: false, cache: true}).pipe(filters());
 
   if (age) stream = stream.pipe(filters.after(age));
 
@@ -30,4 +27,37 @@ function execute(options) {
   }
 
   return stream;
+}
+
+//{id, subject, count, original-from: 'foo@bar.com', latest-from: 'foo@bar.com', start, end}
+//{
+//  $set: { id, subject },
+//  $inc: { count },
+//  $setOnInsert: { start, original-from, end, latest-from }
+//}
+
+var mongojs = require('mongojs');
+// { _id: path, subjectID, subject, date, from }
+function topics(options) {
+  var source = options.source || 'https://mail.mozilla.org/pipermail/es-discuss/';
+  var db = mongojs(options.db, ['messages', 'topics']);
+
+  var running = 0;
+  var stream = pipermail(source, {progress: false, cache: true}).pipe(filters());
+    .pipe(through(function (message) {
+      if (running++ === 0) this.pause();
+      db.messages.create({
+        _id: message.path,
+        subjectID: tag(message.header.subject)
+      }, function (err) {
+        console.error(err.stack || err.message || err);
+        if (--running === 0) this.resume();
+      });
+    }));
+  stream.on('error', function (e) {
+    console.error(e.stack || e.message || e);
+  });
+}
+function tag(subject) {
+  return subject.replace(/[^a-z]+/gi, '').replace(/fwd?/gi, '').replace(/re/gi, '');
 }
