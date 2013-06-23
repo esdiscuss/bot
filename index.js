f31b408155d5644328f7a0e2c23f4f310cf43fc6
ForbesLindesay <forbes@lindesay.co.uk>
2013-06-23 03:59:35 +0100

    Upgrade to pipermail 2.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var pipermail = require('pipermail');
-var filters = require('./lib/pipermail-filters');
-var writeGitHub = require('./lib/write-github');
+var filter = require('./lib/filter');
+var after = require('./lib/after');
 var writeMongo = require('./lib/write-mongo');
 
 exports = (module.exports = messages);
@@ -9,35 +9,19 @@ function messages(options) {
   var source = options.source || 'https://mail.mozilla.org/pipermail/es-discuss/';
   source = source.replace(/\/?$/g, '/');
   var age = options.age;
-  var organisation = options.organisation;
   var dryRun = options['dry-run'] || options.dryRun;
   var db = options.db || null;
 
-  var stream = pipermail(source, {progress: options.progress || false, cache: options.cache !== false, gzip: options.gzip || false}).pipe(filters());
+  var stream = pipermail(source).pipe(filter());
 
   if (age) {
     stream.on('error', error);
-    stream = stream.pipe(filters.after(age));
+    stream = stream.pipe(after(age));
   }
 
   if (db && !dryRun) {
     stream.on('error', error);
     stream = stream.pipe(writeMongo(db));
-  }
-
-  if (organisation) {
-    stream.on('error', error);
-    stream = stream.pipe(filters.notExists(organisation));
-  }
-
-  if (options.user && options.pass && !dryRun) {
-    stream.on('error', error);
-    stream = stream
-      .pipe(writeGitHub({
-          user: {type: 'basic', username: options.user, password: options.pass},
-          organisation: organisation,
-          team: options.team
-        }));
   }
 
   function error(err) {
