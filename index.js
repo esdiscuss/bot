c2d7163d60cd3d5245411377219a78199637bc6e
ForbesLindesay <forbes@lindesay.co.uk>
2013-11-09 14:07:24 +0000

    Filter each message's existence
    This should result in a huge speedup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+'use strict'
+
 var pipermail = require('pipermail')
-var barrage = require('barrage')
-var filter = require('./lib/filter')
+var mongojs = require('mongojs')
+var Promise = require('promise')
 var writeMongo = require('./lib/write-mongo')
 
 module.exports = messages
@@ -11,11 +13,41 @@ function messages(options) {
   var dryRun = options['dry-run'] || options.dryRun
   var db = options.db || null
 
-  var stream = pipermail(source, options).syphon(filter())
+  if (db) {
+    if (typeof db === 'string') db = mongojs(db, ['headers', 'contents', 'topics'])
+    options.filterMessage = function (url) {
+      return new Promise(function (resolve, reject) {
+        db.headers.findOne({url: url}, function (err, res) {
+          if (err || res === null) return resolve(true)
+          db.contents.findOne({_id: res._id}, function (err, res) {
+            if (err || res === null) return resolve(true)
+            return resolve(false)
+          })
+        })
+      })
+    }
+  }
+
+  var stream = pipermail(source, options).filter(function (message) {
+    console.dir(message)
+    //filter spam
+    if (/spam/i.test(message.header.subject) || /no subject/i.test(message.header.subject)) {
+      return false
+    }
+    // update/add some extra properties
+    message.url = message.url.replace(/\/+([^\/]+)\/+([^\/]+)\/+([^\/]+)\/+([^\/]+)$/, '/$1/$2/$3/$4')
+    message.id = message.header.date.toISOString() + '-' + message.header.from.email.replace(/@/g, '.')
+    message.subjectID = tag(message.header.subject)
+    return true
+  })
 
   if (db && !dryRun) {
     stream = stream.syphon(writeMongo(db))
   }
 
   return stream;
+}
+
+function tag(subject) {
+  return subject.replace(/[^a-z]+/gi, '').replace(/fwd?/gi, '').replace(/re/gi, '').toLowerCase()
 }
