d8b3f42482d62654f0fba4b7610fba4fe379fb1c
Forbes Lindesay <forbes@lindesay.co.uk>
2014-02-22 00:34:05 +0000

    Error reporting and refactoring

--- index.js
+++ index.js
@@ -1,73 +1,12 @@
-'use strict'
+'use strict';
 
-var util = require('util')
-var assert = require('assert')
-var pipermail = require('pipermail')
-var mongojs = require('mongojs')
-var Promise = require('promise')
-var writeMongo = require('./lib/write-mongo')
+var Promise = require('promise');
+var readPipermail = require('./lib/read-pipermail');
+var writeMongo = require('./lib/write-mongo');
 
-module.exports = messages
-function messages(options) {
-  options = options || {}
-  var source = options.source || 'https://mail.mozilla.org/pipermail/es-discuss/'
-  source = source.replace(/([^\/])\/?$/g, '$1/')
-  var dryRun = options['dry-run'] || options.dryRun
-  var db = options.db || null
-  var dbAsString = typeof db === 'string';
-  if (db) {
-    if (dbAsString)
-      db = mongojs(db, ['headers', 'contents', 'topics'])
-    options.filterMessage = function (url) {
-      return new Promise(function (resolve, reject) {
-        db.headers.findOne({url: url}, function (err, res) {
-          if (err || res === null) {
-            console.log('header missing: ' + util.inspect(url))
-            if (err) console.error(err.stack || err)
-            return resolve(true)
-          }
-          db.contents.findOne({_id: res._id}, function (err, res) {
-            if (err || res === null) {
-              console.log('body missing: ' + util.inspect(url))
-              if (err) console.error(err.stack || err)
-              return resolve(true)
-            }
-            return resolve(false)
-          })
-        })
-      })
-    }
-  }
-
-  var stream = pipermail(source, options).filter(function (message) {
-    //filter spam
-    if (/spam/i.test(message.header.subject) || /no subject/i.test(message.header.subject)) {
-      return false
-    }
-    // update/add some extra properties
-    message.url = message.url.replace(/\/+([^\/]+)\/+([^\/]+)\/+([^\/]+)\/+([^\/]+)$/, '/$1/$2/$3/$4')
-    message.id = message.header.date.toISOString() + '-' + message.header.from.email.replace(/@/g, '.')
-    message.subjectID = tag(message.header.subject)
-    return true
-  })
-
-  if (db && !dryRun) {
-    stream = stream.syphon(writeMongo(db, options))
-  }
-
-  var closed = false
-  function onEnd() {
-    if (closed) return
-    closed = true
-    db.close()
-  }
-  if (dbAsString) {
-    stream.once('end', onEnd)
-  }
-
-  return stream;
-}
-
-function tag(subject) {
-  return subject.replace(/[^a-z]+/gi, '').replace(/fwd?/gi, '').replace(/re/gi, '').toLowerCase()
+module.exports = doRun;
+function doRun(options) {
+  var source = readPipermail(options);
+  var sink = writeMongo(options);
+  return source.syphon(sink).wait();
 }
--- server.js
+++ server.js
@@ -1,58 +1,81 @@
-var ms = require('ms')
-var mongojs = require('mongojs')
-var bot = require('./index.js')
+'use strict';
 
-var src = process.env.PIPERMAIL_SOURCE || 'https://mail.mozilla.org/pipermail/es-discuss/'
-var db = process.env.PIPERMAIL_DATABASE
-var connection
-if (db) {
-  connection = mongojs(db, ['log', 'headers', 'contents', 'topics'])
+var ms = require('ms');
+var mongojs = require('mongojs');
+  var raven = require('raven');
+var bot = require('./index.js');
+
+if (!process.env.PIPERMAIL_SOURCE) {
+  throw new Error('You must specify the "PIPERMAIL_SOURCE" environment variable');
+}
+if (!process.env.PIPERMAIL_DATABASE) {
+  throw new Error('You must specify the "PIPERMAIL_DATABASE" environment variable');
 }
 
-var didSomething = new Date()
+var src = process.env.PIPERMAIL_SOURCE;
+var db = mongojs(process.env.PIPERMAIL_DATABASE, ['log', 'headers', 'contents', 'topics']);
+
 
 var settings = 'last-reboot:  ' + (new Date()).toISOString() + '\n' +
                'source:       ' + src + '\n' +
-               'database:     ' + (db || 'no database attached').replace(/^.*@/,'')
+               'database:     ' + process.env.PIPERMAIL_DATABASE.replace(/^.*@/,'')
 var lastRun = 'no old runs to display'
 var lastStart = 'never started'
 var lastEnd = 'never finished'
-var lastMessage = 'no messages processsed yet'
+
+var ravenClient = process.env.PIPERMAIL_RAVEN ?
+    new raven.Client(process.env.PIPERMAIL_RAVEN) :
+    null;
+if (ravenClient) {
+  ravenClient.patchGlobal();
+}
+function onError(err) {
+  console.error(err.stack || err.message || err);
+  if (ravenClient) {
+    if (typeof err === 'string') {
+      ravenClient.captureMessage(err);
+    } else {
+      ravenClient.captureError(err);
+    }
+  }
+}
 
 function run() {
-  lastStart = (new Date()).toISOString()
-  var defaultMonths = (new Date()).getDate() < 5 ? 2 : 1
-  didSomething = new Date()
-  return bot({source: src, db: connection, months: +(process.env.PIPERMAIL_MONTHS || defaultMonths), parallel: +(process.env.PIPERMAIL_PARALLEL || 1)})
-    .on('data', function (message) {
-      lastMessage = message.id
-      didSomething = new Date()
-    })
-    .wait()
-    .then(function () {
-      didSomething = new Date()
-      lastEnd = (new Date()).toISOString()
-      if (connection) {
-        connection.log.insert({
-          type: 'bot-run',
-          start: new Date(lastStart),
-          end: new Date(lastEnd)
-        }, {safe: true}, function (err) {
-          if (err) {
-            console.error(err.stack || err.message || err)
-          }
-        })
-      }
-    })
+  lastStart = (new Date()).toISOString();
+  var defaultMonths = process.env.PIPERMAIL_MONTHS || ((new Date()).getDate() < 5 ? 2 : 1);
+  var parallel = process.env.PIPERMAIL_PARALLEL || 1
+  return bot({
+    source: src,
+    db: db,
+    months: +defaultMonths,
+    parallel: +parallel,
+    onError: onError
+  }).then(function () {
+    lastEnd = (new Date()).toISOString()
+    if (db) {
+      db.log.insert({
+        type: 'bot-run',
+        start: new Date(lastStart),
+        end: new Date(lastEnd)
+      }, {safe: true}, function (err) {
+        if (err) {
+          onError(err)
+        }
+      })
+    }
+  });
 }
 maintain()
 function maintain() {
   run().done(function () {
     if (lastEnd != 'never finished') {
-      lastRun = lastStart + ' to ' + lastEnd
+      lastRun = ms(new Date(lastEnd).getTime() - new Date(lastStart).getTime());
     }
-    setTimeout(maintain, 30 * 1000)
-  }, maintain)
+    setTimeout(maintain, ms('30s'));
+  }, function (err) {
+    onError(err);
+    setTimeout(maintain, ms('30s'));
+  })
 }
 
 var http = require('http')
@@ -63,25 +86,19 @@ http.createServer(function (req, res) {
     status = 503
   } else if (Date.now() - (new Date(lastEnd)).getTime() > ms('5 minutes')) {
     status = 503
+    onError('Timeout triggering restart');
+    setTimeout(function () {
+      // allow time for the error to be logged
+      process.exit(1);
+    }, 500);
   }
   res.writeHead(status, {'Content-Type': 'text/plain'})
   var warning = status === 503 ? 'WARNING: server behind on processing\n\n' : ''
   res.end(warning + settings + '\n\n' +
           'last-start:   ' + lastStart + '\n' +
           'last-end:     ' + lastEnd + '\n' +
-          'last-message: ' + lastMessage + '\n' +
           'pervious-run: ' + lastRun + '\n\n' +
-          'current-time: ' + (new Date()).toISOString())
-
-  if ((lastEnd !== 'never finished' && timeSpan(lastEnd, '5 minutes')) || timeSpan(didSomething, '10 minutes')) {
-    console.log('It\'s been way too long...rebooting.')
-    process.exit(1)
-  }
-}).listen(3000)
-function timeSpan(time, span) {
-  var now = new Date()
-  if (typeof time === 'string') time = new Date(time)
-  return now.getTime() - time.getTime() > ms(span.toString())
-}
+          'current-time: ' + (new Date()).toISOString());
+}).listen(3000);
 
 console.log('Server running at http://localhost:3000/');
